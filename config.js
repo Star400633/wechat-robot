d126d4bac67f5bf87cc8c58b972b375f295b0424
baixiangguo <baixiangguo@2dfire.com>
2019-03-08 10:42:56 +0800

    修改配置

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     LOCATION: 'gongshu-district',//收信者所在区（可以访问墨迹天气网站后，查询区的英文拼写）
     NAME: '百香果',//备注姓名
     NICKNAME: 'Star93', //昵称
-    ROOMNAME: ['测试群', 'Bug四大侠', '我消失了'], //群名称
-    SCHEDULE: '00 00 8 * * *',//定时发送时间
+    ROOMNAME: ['测试群', '测试群'], //群名称
+    SCHEDULE: '00 44 10 * * *',//定时发送时间
 }
