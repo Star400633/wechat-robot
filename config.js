f363694e8c7c32b7274fdbf32dfbb689edc8cb19
baixiangguo <baixiangguo@2dfire.com>
2019-03-08 11:02:04 +0800

    恢复配置

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // 配置文件
 module.exports = {
     ONE: 'http://wufazhuce.com/',////ONE的web版网站
-    MOJI_HOST: 'https://tianqi.moji.com/weather/china/', //中国墨迹天气url
+    MOJI_HOST: 'https://tianqi.moji.com/weather/china/', //墨迹天气url
     CITY: 'zhejiang',//收信者所在城市
     LOCATION: 'gongshu-district',//收信者所在区（可以访问墨迹天气网站后，查询区的英文拼写）
     NAME: '百香果',//备注姓名
     NICKNAME: 'Star93', //昵称
-    ROOMNAME: ['测试群', '测试群'], //群名称
-    SCHEDULE: '00 59 10 * * *',//定时发送时间
+    ROOMNAME: ['测试群', 'Bug四大侠', '我消失了'], //群名称
+    SCHEDULE: '0 0 8 * * *',//定时发送时间
 }
