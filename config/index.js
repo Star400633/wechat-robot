c14d54698caa33e557dfebb2148d5e5b3ca5f61a
baixiangguo <baixiangguo@2dfire.com>
2019-04-15 23:32:31 +0800

    添加群推送

--- config/index.js
+++ config/index.js
@@ -1,20 +1,11 @@
 // 配置文件
 module.exports = {
-    AUTOADDFRIEND: false,//自动加好友功能  默认关闭
-    AUTOADDROOM: false,//自动拉群功能 默认关闭
     YOUDAO: 'http://dict.youdao.com/infoline/style/cardList?style=daily&apiversion=3.0&client=mobile&lastId=0&size=16&preSize=0&keyfrom=mdict.7.8.9.android&model=CLT-AL00&mid=9&imei=869714031552066&vendor=zhihuiyun&screen=1080x2159&ssid=%3Cunknown+ssid%3E&abtest=3', // 有道壹句
-    ONE: 'http://wufazhuce.com/',////ONE的web版网站
     MOJI_HOST: 'https://tianqi.moji.com/weather/china/', //墨迹天气url
     CITY: 'zhejiang',//收信者所在城市
     LOCATION: 'gongshu-district',//收信者所在区 （可以访问墨迹天气网站后，查询区的英文拼写）
-    MEMORIAL_DAY: '2013/04/18', //你和收信者的纪念日
-    NAME: '明月',//备注姓名
-    NICKNAME: 'W _cloudy明月🍓', //昵称
-    ROOMNAME: '测试群', //群名称
+    NICKNAME_LIST: ['W _cloudy明月🍓'], // 昵称列表
+    ROOMNAME: ['Bug四大侠'], //群名称
     SENDDATE: '0 0 8 * * *',//定时发送时间 每天8点15分30秒发送，规则见 /schedule/index.js
-    ROOMNAME: '/^啦啦啦咔咔/i', //群名(请只修改中文，不要删除符号，这是正则)
-    ADDFRIENDWORD: '/你要触发的关键词/i',//自动加好友触发的关键词(请只修改中文，不要删除符号，这是正则)
-    ADDROOMWORD: '/加群/',//自动发送群图片触发关键词(请只修改中文，不要删除符号，这是正则)
-    ROOMCODEURL: 'http://image.bloggeng.com/qun.png',//群二维码url链接(与本地群二维码路径选填一个)
-    ROOMLOCALPATH: './static/qun.png',//本地群二维码图片路径（与群url选填一个）
+    HEARTBEAT: '0 0 * * * *', // 心跳 每个整点
 }
--- main.js
+++ main.js
@@ -21,9 +21,6 @@ const ctx = canvas.getContext('2d')
 bot.on('scan', onScan)
 bot.on('login', onLogin)
 bot.on('logout', onLogout)
-bot.on('message', onMessage)
-bot.on('friendship', onFriendShip)
-bot.on('room-join', roomJoin)
 
 bot.start()
 .then(() => console.log('开始登陆微信'))
@@ -44,13 +41,15 @@ async function onLogin(user) {
         schedule.setSchedule(config.SENDDATE, async () => {
             await main()
         })
+        schedule.setSchedule(config.HEARTBEAT, () => {
+            console.log(`现在是：${new Date()} 心跳正常~`)
+        })
     }
 }
 
 // 自动发消息功能
 async function main() {
     console.log('开始工作~')
-    let contact = await bot.Contact.find({name: config.NICKNAME}) || await bot.Contact.find({alias: config.NAME}) // 获取你要发送的联系人
     
     let one = await tasks.getOne() //获取每日一句
     let weather = await tasks.getWeather() //获取天气信息
@@ -83,14 +82,26 @@ async function main() {
             .write(resultimg, async (err) => {
                 if (err) console.log('error')
                 
-                let logMsg = FileBox.fromFile(resultimg)
+                let sendMessage = FileBox.fromFile(resultimg)
                 
-                if(logMsg) {
-                    await contact.say(logMsg) // 发送消息
-                    fs.unlinkSync(fileName)
-                    console.log('发送成功~')
+                if(sendMessage) {
+                    // 联系人
+                    for(const item of config.NICKNAME_LIST) {
+                        let contact = await bot.Contact.find({name: item}) // 获取你要发送的联系人
+                        await contact.say(sendMessage) // 发送消息
+                    }
+                    
+                    // 群
+                    for(const item of config.ROOMNAME) {
+                        let room = await bot.Room.find({topic: item})
+                        console.log(room)
+                        await room.say(sendMessage)
+                    }
+    
+                    fs.unlinkSync(fileName) // 删除文件
+                    
                 } else {
-                    console.log('error', logMsg)
+                    console.log('error', sendMessage)
                 }
             })
             
@@ -100,8 +111,6 @@ async function main() {
         }
     })
 }
-
-
 
 //  二维码生成
 function onScan(qrcode, status) {
@@ -118,89 +127,3 @@ function onLogout(user) {
     console.log(`${user} 登出`)
 }
 
-// 监听对话 根据关键词自动加群
-async function onMessage(msg) {
-    const contact = msg.from() // 发消息人
-    const content = msg.text() //消息内容
-    const room = msg.room() //是否是群消息
-    const roomCodeUrl = FileBox.fromUrl(config.ROOMCODEURL) //来自url的文件
-    const roomCodeLocal = FileBox.fromFile(config.ROOMLOCALPATH) //添加本地文件
-    if(msg.self()) {
-        return
-    }
-    if(room) { // 如果是群消息
-        const topic = await room.topic()
-        // console.log(`群名: ${topic} 发消息人: ${contact.name()} 内容: ${content}`)
-    } else { // 如果非群消息
-        // console.log(`发消息人: ${contact.name()} 消息内容: ${content}`)
-        if(config.AUTOADDROOM) { //判断是否开启自动加群功能
-            let addRoomReg = eval(config.ADDROOMWORD)
-            let roomReg = eval(config.ROOMNAME)
-            if(addRoomReg.test(content) && !room) {
-                let keyRoom = await this.Room.find({topic: roomReg})
-                if(keyRoom) {
-                    try {
-                        await contact.say(roomCodeLocal || roomCodeUrl)
-                    } catch (e) {
-                        console.error(e)
-                    }
-                }
-            } else {
-                await contact.say('你好，不要轻易调戏我，我只会发群二维码，不会聊天的！')
-                await contact.say('请回复暗号：加群  获取群二维码图片')
-            }
-        }
-    }
-}
-
-// 自动加好友功能
-async function onFriendShip(friendship) {
-    let logMsg
-    try {
-        logMsg = '添加好友' + friendship.contact().name()
-        console.log(logMsg)
-        
-        switch (friendship.type()) {
-            /**
-             *
-             * 1. New Friend Request
-             *
-             * when request is set, we can get verify message from `request.hello`,
-             * and accept this request by `request.accept()`
-             */
-            case Friendship.Type.Receive:
-                let addFriendReg = eval(config.ADDFRIENDWORD)
-                if(addFriendReg.test(friendship.hello()) && config.AUTOADDFRIEND) { //判断是否开启自动加好友功能
-                    logMsg = '自动添加好友，因为验证信息中带关键字‘每日说’'
-                    await friendship.accept()
-                } else {
-                    logMsg = '没有通过验证 ' + friendship.hello()
-                }
-                break
-            /**
-             *
-             * 2. Friend Ship Confirmed
-             *
-             */
-            case Friendship.Type.Confirm:
-                logMsg = 'friend ship confirmed with ' + friendship.contact().name()
-                break
-        }
-    } catch (e) {
-        logMsg = e.message
-    }
-    console.log(logMsg)
-}
-
-
-// 加群提醒
-function roomJoin(room, inviteeList, inviter) {
-    const nameList = inviteeList.map(c => c.name()).join(',')
-    room.topic().then(function(res) {
-        const roomNameReg = eval(config.ROOMNAME)
-        if(roomNameReg.test(res)) {
-            console.log(`群名： ${res} ，加入新成员： ${nameList}, 邀请人： ${inviter}`)
-            room.say(`${res}：欢迎新朋友 @${nameList}，<br>使用过程中有什么问题都可以在群里提出`)
-        }
-    })
-}
